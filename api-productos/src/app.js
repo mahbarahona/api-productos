7e9cb55bb7151cff655b42e2368a566181720996
Manuel Hernandez <mahbarahona@gmail.com>
2022-03-25 08:19:43 -0400

    first commit

---
+++ api-productos/src/app.js
@@ -0,0 +1,38 @@
+
+const express        = require('express');
+const cors           = require('cors');
+const helmet         = require('helmet');
+const morgan         = require('morgan');
+
+
+
+
+const app = express();
+
+//configurar mw
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(cors());
+
+
+
+app.use('/api',( req, res) =>{
+    const response = {
+        status:true,
+    }
+    console.log('response',response)
+    res
+    .status(200)
+    .header({})
+    .send(response)
+});
+
+
+//server
+const puerto = process.env.PORT || 3000;
+
+app.listen( puerto, ()=>{
+    console.log(`Servidor en puerto ${puerto},http://localhost:${puerto}`)
+})
+
+
